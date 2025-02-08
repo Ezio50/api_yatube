65e2ed6d12c246acaf40c4eede7ce6770208f00b
Ezio50 <135201325+Ezio50@users.noreply.github.com>
2025-02-08 18:05:00 +0700

    Update permissions.py

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -1,13 +1,12 @@
 from rest_framework import permissions
 
+
 class IsAuthorOrReadOnly(permissions.BasePermission):
     """
-    Разрешение: изменение/удаление объекта разрешено только автору.
-    Остальные запросы – только для чтения.
+    Разрешение: редактировать может только автор, остальные - только читать.
     """
+
     def has_object_permission(self, request, view, obj):
-        # Безопасные методы (GET, HEAD, OPTIONS) разрешены всем
         if request.method in permissions.SAFE_METHODS:
             return True
-        # Изменять можно только если пользователь – автор
         return obj.author == request.user
