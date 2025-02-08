9d5b19bba57ee4c230732597085103f0abfd8140
Ezio50 <135201325+Ezio50@users.noreply.github.com>
2025-02-08 18:04:42 +0700

    Update urls.py

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,20 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from rest_framework.authtoken.views import obtain_auth_token
-from .views import PostViewSet, GroupViewSet, CommentViewSet
+from .views import PostViewSet
 
 router = DefaultRouter()
-router.register(r'posts', PostViewSet, basename='posts')
-router.register(r'groups', GroupViewSet, basename='groups')
-# При регистрации вьюсета для комментариев используем регулярное выражение для post_id.
-router.register(
-    r'posts/(?P<post_id>\d+)/comments',
-    CommentViewSet,
-    basename='post-comments'
-)
+router.register(r'posts', PostViewSet)
 
 urlpatterns = [
-    # Эндпоинт для получения токена: POST api/v1/api-token-auth/
-    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
     path('', include(router.urls)),
 ]
